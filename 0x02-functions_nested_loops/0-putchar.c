b18a83d5a75fa9b0d08a14abc481ce31e0e0c588
Kgaobella <rphukubye@yahoo.com>
2023-02-21 10:14:41 -0800

    Task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,7 @@
 /**
  * main - print _putchar
  * Written by Rapela
- *Return: 0
+ *Return: Always 0
  */
 
 int main(void)
@@ -16,6 +16,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n')
+	_putchar('\n');
 	return (0);
 }
